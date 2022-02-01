60c6f05bbf6abbf1e6bdfab6ad31bf9dd559eb2b
sanket.sud <sanketsud21@gmail.com>
2022-02-01 18:54:37 +0000

    init

---
+++ mongodb/connection.js
@@ -0,0 +1,17 @@
+const MongoClient =require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://admin:password@localhost:27017', function(err, client) {
+    if (err) throw err;
+
+        console.log("connection success");
+    
+        var db ='user-account';
+        var query = { _id: 10, item: "box", qty: 20 }
+        db.collection('user').update(query, (err, res) => {
+            if (err) throw err
+            console.log(res)
+        })
+
+    
+
+})
